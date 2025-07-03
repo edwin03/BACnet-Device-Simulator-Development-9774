be5c648c3344c2fa4ae6c11bf2826465887d0f79
Edwin Rubio <rubioed@oregonstate.edu>
2025-07-03 15:48:48 -0500

    Added files via Greta

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,19 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   base: './',
+  server: {
+    port: 5174
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
   },
-   build: {
+  build: {
     outDir: 'dist',
     sourcemap: true
   },
